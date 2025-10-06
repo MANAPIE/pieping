27d1dfe5769249c2af2777535cb8610676ab1de8
MANAPIE <bakery@manapie.me>
2025-10-06 21:11:37 +0900

    Initial Commit

---
+++ pieping/piepingApp.swift
@@ -0,0 +1,17 @@
+//
+//  piepingApp.swift
+//  pieping
+//
+//  Created by MANAPIE on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct piepingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
